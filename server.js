d19296a164a7140cb1e5b185db9d497e7f321bb7
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2024-01-28 10:15:39 +0100

    ... - 28.01.2024 10:15

--- server.js
+++ server.js
@@ -6,32 +6,26 @@ var fs = require('fs')
 var app = connect()
 
 app.use(bodyParser.urlencoded({
-    extended : false
+    extended :false
 }))
 
 app.use((req, res) => {
-    if(req.method == "GET") {
-        var url = (req.url == '/') ? '/index.html' : req.url
-        var filename = __dirname + "/public" + url
+    var url = (req.url == '/') ? '/index.html' : req.url
+    var filename = __dirname + '/public' + url
+    console.log(filename)
+    fs.readFile(filename, 'utf-8', (err, data) => {
+        if (err) {
+            res.end("Page Not Found")
+        }
+        else {
+            res.end(data)
+        }
+    })
+    
 
-        console.log(filename)
-
-        fs.readFile(filename, "utf-8", (err, data) => {
-            if(err) {
-                // res.setHeader(404)
-                res.end("Page Not Found")
-            }
-            else {
-                res.end(data);
-            }
-        })
-        // res.end("GET method used !")
-    }
-    else {
-        res.end('GET method not used !')
-    }
+    // res.end("Hello World !")
 })
 
 http.createServer(app).listen(3000)
 
-console.log('HERE WE GO !!!')
+console.log("HERE WE GO !!!")
